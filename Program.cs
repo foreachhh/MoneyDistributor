7960d1035039769c880f7cd195f66bf786d25077
foreachhh <foreach26@yandex.ru>
2025-11-03 14:32:12 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyDistributor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
